0057e671f240e70c6fccbed9e9ec323f53add4d2
archurr0 <aliang4860@gmail.com>
2024-04-06 22:45:37 -0700

    update allowed origins on cors

--- server/config/allowedOrigins.js
+++ server/config/allowedOrigins.js
@@ -2,5 +2,6 @@ const allowedOrigins = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
     "http://192.168.50.199:5000",
+    "https://portfolio-mern-1.onrender.com",
   ];
   module.exports = allowedOrigins;
